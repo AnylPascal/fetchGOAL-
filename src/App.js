a349701dead944d608749c51e8aec3fd610c66e9
AnylPascal <info@anylpascal.com>
2020-08-03 11:06:13 -0400

    api key working

--- src/App.js
+++ src/App.js
@@ -4,26 +4,70 @@ import './App.css';
 import { Route, Link } from 'react-router-dom';
 import Dashboard from './Dashboard';
 
+
+const apiKey = process.env.REACT_APP_MYKEY;
+
 class App extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
 			data: {},
+			loading: true,
+			lineUps: {},
 		};
 	}
 
-	setState = {
-		loading: true,
-		data: {},
-	};
+	componentDidMount() {
+		fetch('https://api-football-v1.p.rapidapi.com/v2/predictions/157462', {
+			method: 'GET',
+			headers: {
+				'x-rapidapi-host': 'api-football-v1.p.rapidapi.com',
+				'x-rapidapi-key': apiKey,
+			},
+		})
+			.then((response) => {
+				console.log(response);
 
-	async componentDidMount() {
-		const url = 'https://api-football-v1.p.rapidapi.com/v2/predictions/157462';
-		const response = await fetch(url);
-		const data = await response.json();
+				return response.json();
+			})
+			.then((response) => {
+				console.log(response);
+
+				this.setState({
+					...this.state,
+					loading: false,
+					data: response.api.predictions[0],
+				});
+			});
+
+		fetch('https://api-football-v1.p.rapidapi.com/v2/lineups/157215', {
+			method: 'GET',
+			headers: {
+				'x-rapidapi-host': 'api-football-v1.p.rapidapi.com',
+				'x-rapidapi-key': apiKey,
+			},
+		})
+			.then((response) => {
+				console.log(response);
+
+				return response.json();
+			})
+			.then((response) => {
+				console.log(response.api.lineUps);
+
+				this.setState({
+					...this.state,
+					lineUps: response.api.lineUps,
+				});
+			})
+			.catch((err) => {
+				console.log(err);
+			});
 	}
 
 	render() {
+
+
 		return (
 			<div className='App'>
 				<div className='App-header'>
--- src/Thecall.js
+++ src/Thecall.js
@@ -1,34 +1,35 @@
 import React, { Component } from 'react';
 
+
+
+
 class Thecall extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			info: {},
+		};
+	}
+
+	componentDidMount() {
+		fetch('https://api-football-v1.p.rapidapi.com/v2/lineups/157215', {
+			method: 'GET',
+			headers: {
+				'x-rapidapi-host': 'api-football-v1.p.rapidapi.com',
+				'x-rapidapi-key': 'cecc3a01f3msh3ad1f8c78be3775p163da2jsn14d409d6beee',
+			},
+		})
+			.then((response) => {
+				console.log(response);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}
+
 	render() {
-		return (
-			<div>
-				
-			</div>
-		);
+		return <div></div>;
 	}
 }
-
-export default Thecall;
-
-var unirest = require('unirest');
-
-var req = unirest(
-	'GET',
-	'https://api-football-v1.p.rapidapi.com/v2/lineups/157215'
-);
-
-req.headers({
-	'x-rapidapi-host': 'api-football-v1.p.rapidapi.com',
-	'x-rapidapi-key': 'cecc3a01f3msh3ad1f8c78be3775p163da2jsn14d409d6beee',
-	useQueryString: true,
-});
-
-req.end(function (res) {
-	if (res.error) throw new Error(res.error);
-
-	console.log(res.body);
-});
 
 export default Thecall;
