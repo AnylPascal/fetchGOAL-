471925518e54aab50aec54ca98ef2c68595171e2
AnylPascal <info@anylpascal.com>
2020-08-14 17:06:36 -0400

    updates config of API routes

--- src/Standings.js
+++ src/Standings.js
@@ -1,8 +1,24 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-class Dashboard extends Component {
+class Standings extends Component {
 	render() {
-		
+		const api = [];
+
+		for (const where in this.props.api.standings) {
+			api.push({ name: where, ...this.props.standings[where] });
+		}
+		return (
+			<div>
+				{api.map((where) => (
+					<div key={where.api.standings}>
+						{' '}
+						{''} <div>{where.api.standings}</div>{' '}
+					</div>
+				))}{' '}
+				{''}
+			</div>
+		);
+	}
 }
-export default Dashboard;
+export default Standings;
